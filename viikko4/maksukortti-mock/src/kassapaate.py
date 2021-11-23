55c168587c1b229d1231dac37318dde053546c7c
Juan Covarrubias <me@juan.fi>
2021-11-23 11:47:02 +0200

    Week4 Ex2

--- viikko4/maksukortti-mock/src/kassapaate.py
+++ viikko4/maksukortti-mock/src/kassapaate.py
@@ -6,8 +6,14 @@ class Kassapaate:
         self.myytyja_lounaita = 0
 
     def lataa(self, kortti, summa):
+        if summa <= 0:
+            return
+        
         kortti.lataa(summa)
 
     def osta_lounas(self, kortti):
+        # Poistutaan, jos saldo kortilla ei riitä
+        if kortti.saldo < HINTA: return
+
         kortti.osta(HINTA)
         self.myytyja_lounaita = self.myytyja_lounaita + 1
--- viikko4/maksukortti-mock/src/tests/kassapaate_test.py
+++ viikko4/maksukortti-mock/src/tests/kassapaate_test.py
@@ -23,3 +23,18 @@ class TestKassapaate(unittest.TestCase):
         self.kassa.osta_lounas(maksukortti_mock)
 
         maksukortti_mock.osta.assert_not_called()
+
+    def test_kortille_ladataan_saldoa_jos_summa_positiivinen(self):
+        maksukortti_mock = Mock()
+
+        self.kassa.lataa(maksukortti_mock, 2)
+
+        maksukortti_mock.lataa.assert_called_with(2)
+
+    def test_kortille_ei_ladata_saldoa_jos_summa_negatiivinen(self):
+        maksukortti_mock = Mock()
+        
+        self.kassa.lataa(maksukortti_mock, -1)
+
+        maksukortti_mock.lataa.assert_not_called()
+
